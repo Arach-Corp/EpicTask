77dd2a966aa457deaf108b443057ffefc444082e
Joao Queiroz <joaovic.pqueiroz@gmail.com>
2021-08-25 21:28:19 -0300

    login controller, i18n

--- src/main/java/br/com/fiap/epictask/controller/LoginController.java
+++ src/main/java/br/com/fiap/epictask/controller/LoginController.java
@@ -1,14 +1,35 @@
 package br.com.fiap.epictask.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/auth")
 public class LoginController {
 	
-	@RequestMapping("/login")
-	public String login() {
+	@GetMapping("/login")
+	public String loginForm() {
 		return "login";
 	}
-	
+
+	@PostMapping("/login")
+	public String login(){
+		return "login";
+	}
+
+
+
+	@GetMapping("/register")
+	public String registerForm(){
+		return "login";
+	}
+
+	@PostMapping("/register")
+	public String register(){
+		return "login";
+	}
+
+
 }
